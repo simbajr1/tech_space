4a8df35befbe623bacf08a13bf4e4a52eb35cb4a
simbajr1 <ojaphe1999@gmail.com>
2024-10-25 16:02:43 +0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar.js"; // Verify Navbar.js exists
-import Footer from "./components/Footer.js";
-import Home from "./pages/Home"; // Verify Home.js exists
-import "./App.css"
-import "./tailwind.css"
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
